9c23217177e066cca762875bc26acb026324cfcc
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-02-18 15:10:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+    const inputs = document.querySelectorAll('.code');
+
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', (e) => {
+            if (input.value.length === 1) {
+                if (index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                } else {
+                    input.blur();
+                }
+            } else if (input.value.length === 0 && e.inputType === 'deleteContentBackward') {
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
+
+        input.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace' && input.value.length === 0) {
+                e.preventDefault();
+                if (index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
+    });
